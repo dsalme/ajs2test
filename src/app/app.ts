521c9bbb644683964caf50021f826a83298b1ce0
dsalme <dsalme@gmail.com>
2015-11-24 16:48:02 -0300

    added gitignore, tsconfig and first boilerplate, with some npm packages

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import {bootstrap, Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'chabon'
+})
+@View({
+  template: `
+    <div>hola locos</div>
+  `
+})
+
+class Chabon{}
+
+bootstrap(Chabon);
